06a2062b0d3846019877192c3560acc637ca23d5
MateuscMachado <mateuscalabria@gmail.com>
2021-10-01 09:47:48 -0300

    People walking skeleton

--- People/Startup.cs
+++ People/Startup.cs
@@ -6,13 +6,17 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
+using People.Models.Context;
+using People.Services;
+using People.Services.Implementations;
 
-namespace Person
+namespace People
 {
     public class Startup
     {
@@ -28,10 +32,15 @@ namespace Person
         {
 
             services.AddControllers();
-            services.AddSwaggerGen(c =>
-            {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "Person", Version = "v1" });
-            });
+
+            var connection = Configuration["MySQLConnection:MySQLConnectionString"];
+            services.AddDbContext<MySQLContext>(options => options.UseMySql(connection, ServerVersion.AutoDetect(connection)));
+
+            // API Versioning
+            services.AddApiVersioning();
+            
+            // Dependency Injection!!!!
+            services.AddScoped<IPersonService, PersonServiceImplementation>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -40,8 +49,6 @@ namespace Person
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
-                app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Person v1"));
             }
 
             app.UseHttpsRedirection();
